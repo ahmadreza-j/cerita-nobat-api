7d69870e0a96d1579d30e20beccc22910952c297
Ahmadreza <a.joolaee@gmail.com>
2023-07-16 13:09:38 +0330

    v2 before cleaning

--- app.js
+++ app.js
@@ -3,154 +3,60 @@ const cors = require("cors");
 
 const { port } = require("./env");
 const {
-  login,
+  // login,
   getTurnsByDate,
   createTurns,
   editTurn,
   deleteTurn,
 } = require("./database");
-const {
-  getDate,
-  nextDayDate,
-  prevDayDate,
-  getDateRange,
-  getTimeRange,
-} = require("./moment");
-const { generateTurnsByDate } = require("./turn");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/getDate/:date?/:format?", async (req, res, next) => {
-  const date = req.params.date;
-  const format = req.params.format;
+// app.post("/login", async (req, res, next) => {
+//   try {
+//     const user = await login(req.body);
+//     res.status(200).send(user);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// app.get("/", async (req, res, next) => {
+//   res.status(200).send({ message: "everything is OK" });
+// });
+
+app.get("/turns/:rdate?/:cdate?", async (req, res, next) => {
   try {
-    const result = getDate(date, format);
-    res.status(200).send(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get("/nextDayDate/:date?/:format?", async (req, res, next) => {
-  const date = req.params.date;
-  const format = req.params.format;
-  try {
-    const result = nextDayDate(date, format);
-    res.status(200).send(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get("/prevDayDate/:date?/:format?", async (req, res, next) => {
-  const date = req.params.date;
-  const format = req.params.format;
-  try {
-    const result = prevDayDate(date, format);
-    res.status(200).send(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get(
-  "/getDateRange/:startDate?/:endDate?/:format?",
-  async (req, res, next) => {
-    const startDate = req.params.startDate;
-    const endDate = req.params.endDate;
-    const format = req.params.format;
-    try {
-      const result = getDateRange(startDate, endDate, format);
-      res.status(200).send(result);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-app.get(
-  "/getTimeRange/:startTime?/:endTime?/:periodInMinute?/:format?",
-  async (req, res, next) => {
-    const startTime = req.params.startTime;
-    const endTime = req.params.endTime;
-    const periodInMinute = req.params.periodInMinute;
-    const format = req.params.format;
-    try {
-      const result = getTimeRange(startTime, endTime, periodInMinute, format);
-      res.status(200).send(result);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-app.get("/generateTurnsByRange", async (req, res, next) => {
-  const qp = req.query.rangeList;
-  const rangeList = qp ? JSON.parse(qp) : [];
-  try {
-    let turnsList = [];
-    rangeList.forEach((item) =>
-      turnsList.push(
-        ...generateTurnsByDate(
-          item?.date,
-          item?.startTime,
-          item?.endTime,
-          item?.periodInMinute
-        )
-      )
-    );
-    const result = turnsList.length > 0 ? turnsList : generateTurnsByDate();
-    res.status(200).send(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.post("/login", async (req, res, next) => {
-  try {
-    const user = await login(req.body);
-    res.status(200).send(user);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.post("/turns", async (req, res, next) => {
-  try {
-    const turns = await createTurns(req.body);
-    res.status(201).send(turns);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get("/turnsByDate/:date?", async (req, res, next) => {
-  try {
-    const date = req.params.date;
-    const turns = await getTurnsByDate(date);
+    const cdate = req.params.cdate;
+    const rdate = req.params.rdate || "now";
+    const turns = await getTurnsByDate(rdate, cdate);
     res.status(200).send(turns);
   } catch (error) {
     next(error);
   }
 });
 
-app.get("/", async (req, res, next) => {
-  res.status(200).send({ message: "everything is OK" });
+app.post("/turn", async (req, res, next) => {
+  try {
+    const newTurn = await createTurns(req.body);
+    res.status(201).send(newTurn);
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.put("/turn/:id/edit", async (req, res, next) => {
+app.put("/turn", async (req, res, next) => {
   try {
-    const id = req.params.id;
-    const turn = await editTurn(id, req.body);
+    const turn = await editTurn(req.body);
     res.status(200).send(turn);
   } catch (error) {
     next(error);
   }
 });
 
-app.delete("/turn/:id/delete", async (req, res, next) => {
+app.delete("/turn/:id", async (req, res, next) => {
   try {
     const id = req.params.id;
     const turn = await deleteTurn(id);
--- database.js
+++ database.js
@@ -1,11 +1,9 @@
 const mysql = require("mysql2");
-const moment = require("jalali-moment");
 
 const { dbHost, dbUser, dbPass, dbName } = require("./env");
 const { Err } = require("./error");
 
-const { getDate, getTime, getTimeRange } = require("./moment");
-const { generateTurnsByDate, initTrun, prepareTurns } = require("./turn");
+const { handleDate, prepareAllDateFormats } = require("./moment");
 
 const pool = mysql
   .createPool({
@@ -16,174 +14,67 @@ const pool = mysql
   })
   .promise();
 
-const getWorkingTimeList = async () => {
+const getTurnsByDate = async (requestedDate, currentDate) => {
   try {
-    const [rows] = await pool.query(
+    const mDate = handleDate(requestedDate, currentDate);
+    const sqlDate = mDate.enDate;
+
+    const [dbTurns] = await pool.query(
       `
-          SELECT * 
-          FROM workingtimeranges
-          `
+        SELECT
+        *
+        FROM turns
+        WHERE DATE(date) = DATE(?)
+        ORDER BY date ASC
+        `,
+      [sqlDate]
     );
-    return rows;
+    const result = {
+      date: mDate,
+      turns: dbTurns.map((item) => ({
+        ...item,
+        date: handleDate(item.date).faFullDate,
+      })),
+    };
+    return result;
   } catch (error) {
-    return new Err(400, error.sqlMessage);
+    throw new Err(400, error.sqlMessage);
   }
 };
 
-const login = async ({ userNumber }) => {
+const createTurns = async ({ refname, refphone, user, description, date }) => {
   try {
-    const [rows] = await pool.query(
-      `
-          SELECT * 
-          FROM operators
-          WHERE id = ?
-          `,
-      [userNumber]
-    );
-    if (!rows[0]) {
-      throw new Error("اجازه ورود داده نشد!");
-    }
-    return { ...rows[0], message: `${rows[0].name} عزیز خوش آمدید` };
-  } catch (error) {
-    return new Err(400, error.sqlMessage || error.message);
-  }
-};
-
-const createTurns = async ({
-  visitordr = 1,
-  refname,
-  refphone,
-  turner = 1,
-  description,
-  dates,
-}) => {
-  try {
-    const isBatch = dates.length > 1;
-    const items = dates.map((date) => [
-      visitordr,
-      refname,
-      refphone,
-      turner,
-      description,
-      getDate(date).enFullDate,
-      isBatch,
-    ]);
+    const sqlDate = handleDate(date).enFullDate;
+    const items = [refname, refphone, user, description, sqlDate];
     const [result] = await pool.query(
       `
-    INSERT INTO appointments (visitordr, refname, refphone, turner, description, date, isBatch)
+    INSERT INTO turns (refname, refphone, user, description, date)
     VALUES ?
     `,
-      [items]
+      [[items]]
     );
     return result;
   } catch (error) {
     const dupMsg = "خطای ثبت نوبت تکراری";
     const msg = error.code === "ER_DUP_ENTRY" ? dupMsg : error.sqlMessage;
-    return new Err(400, msg);
+    throw new Err(400, msg);
   }
 };
 
-const getTurnById = async (id) => {
+const editTurn = async ({ id, refname, refphone, description, date, user }) => {
   try {
-    const [rows] = await pool.query(
-      `
-        SELECT * 
-        FROM appointments
-        WHERE id = ?
-        `,
-      [id]
-    );
-    return rows[0];
-  } catch (error) {
-    return new Err(400, error.sqlMessage);
-  }
-};
-
-const getTurnsByDate = async (date) => {
-  try {
-    const mDate = getDate(date);
-    const sDate = mDate.faDate;
-    const enSDate = mDate.enDate;
-    const workingTimeRanges = await getWorkingTimeList();
-    let generatedTurns = [];
-    workingTimeRanges.forEach((item) =>
-      generatedTurns.push(
-        ...generateTurnsByDate(
-          sDate,
-          item?.startTime,
-          item?.endTime,
-          item?.periodInMinute
-        )
-      )
-    );
-    const initiatedTurns = generatedTurns.map((item) => initTrun(item));
-
-    const [dbTurns] = await pool.query(
-      `
-      SELECT
-      appointments.id, appointments.refname, appointments.refphone, appointments.description, appointments.date, appointments.status, appointments.isBatch,
-      doctors.name AS doctorName, 
-      operators.name AS operatorName
-      FROM appointments
-      INNER JOIN doctors
-        ON doctors.id = appointments.visitordr
-      INNER JOIN operators
-        ON operators.id = appointments.turner
-      WHERE DATE(date) = DATE(?)
-      `,
-      [enSDate]
-    );
-    dbTurns.forEach((item) => {
-      let dbTurnItemTime = getDate(item.date).time;
-      let initiatedTurn = initTrun(getDate(item.date).faFulldate, item);
-      let i = generatedTurns.findIndex(
-        (el) => dbTurnItemTime === getDate(el).time
-      );
-      if (i !== -1) {
-        initiatedTurns[i] = initiatedTurn;
-      } else {
-        initiatedTurns.push(initiatedTurn);
-      }
-    });
-
-    return prepareTurns(mDate, initiatedTurns);
-  } catch (error) {
-    return new Err(400, error.sqlMessage);
-  }
-};
-
-const getTurnsByRange = async (startDate, endDate) => {
-  try {
-    const [rows] = await pool.query(
-      `
-      SELECT * 
-      FROM appointments
-      WHERE DATE(date) BETWEEN DATE(?) AND DATE(?)
-      `,
-      [startDate, endDate]
-    );
-    return rows;
-  } catch (error) {
-    return new Err(400, error.sqlMessage);
-  }
-};
-
-const editTurn = async (
-  id,
-  { visitordr = 1, refname, refphone, turner = 1, description, date }
-) => {
-  try {
+    const sqlDate = handleDate(date).enFullDate;
     const [result] = await pool.query(
       `
-    UPDATE appointments 
-    SET visitordr = ?, refname = ?, refphone = ?, turner = ?, description = ?, date = ?
+    UPDATE turns
+    SET refname = ?, refphone = ?, description = ?, date = ?, user = ?
     WHERE id = ?
     `,
-      [visitordr, refname, refphone, turner, description, date, id]
+      [refname, refphone, description, sqlDate, user, id]
     );
-    return getTurnById(id);
+    return result;
   } catch (error) {
-    return new Err(400, error.sqlMessage);
+    throw new Err(400, error.sqlMessage);
   }
 };
 
@@ -191,24 +82,74 @@ const deleteTurn = async (id) => {
   try {
     const [result] = await pool.query(
       `
-    DELETE FROM appointments
+    DELETE FROM turns
     WHERE id = ?
     `,
       [id]
     );
+    if (!result.affectedRows) {
+      throw { code: 404, message: "آیتم مورد نظر یافت نشد" };
+    }
     return result;
   } catch (error) {
-    return new Err(400, error.sqlMessage);
+    throw new Err(error.code || 400, error.sqlMessage || error.message);
   }
 };
 
+// const login = async ({ userNumber }) => {
+//   try {
+//     const [rows] = await pool.query(
+//       `
+//           SELECT *
+//           FROM operators
+//           WHERE id = ?
+//           `,
+//       [userNumber]
+//     );
+//     if (!rows[0]) {
+//       throw new Error("اجازه ورود داده نشد!");
+//     }
+//     return { ...rows[0], message: `${rows[0].name} عزیز خوش آمدید` };
+//   } catch (error) {
+//     return new Err(400, error.sqlMessage || error.message);
+//   }
+// };
+
+// const getTurnById = async (id) => {
+//   try {
+//     const [rows] = await pool.query(
+//       `
+//         SELECT *
+//         FROM appointments
+//         WHERE id = ?
+//         `,
+//       [id]
+//     );
+//     return rows[0];
+//   } catch (error) {
+//     return new Err(400, error.sqlMessage);
+//   }
+// };
+
+// const getTurnsByRange = async (startDate, endDate) => {
+//   try {
+//     const [rows] = await pool.query(
+//       `
+//       SELECT *
+//       FROM appointments
+//       WHERE DATE(date) BETWEEN DATE(?) AND DATE(?)
+//       `,
+//       [startDate, endDate]
+//     );
+//     return rows;
+//   } catch (error) {
+//     return new Err(400, error.sqlMessage);
+//   }
+// };
+
 module.exports = {
-  login,
-  getWorkingTimeList,
-  createTurns,
-  getTurnById,
   getTurnsByDate,
-  getTurnsByRange,
+  createTurns,
   editTurn,
   deleteTurn,
 };
