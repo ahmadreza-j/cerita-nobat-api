31de3e788388ddbdd0ae5d98e880210e2b5b1b62
Ahmadreza <a.joolaee@gmail.com>
2023-08-25 01:43:42 +0330

    remove redirect

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Redirect HTTP to HTTPS
-app.use((req, res, next) => {
-  if (req.secure) {
-    next();
-  } else {
-    res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-});
-
 // app.post("/login", async (req, res, next) => {
 //   try {
 //     const user = await login(req.body);
